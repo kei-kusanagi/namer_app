c67dcff0662bb3751114100c8dbbf48fafe53a45
kei-kusanagi <kei_kusanagi@outlook.com>
2023-04-13 12:59:05 -0600

    use provider to change color theme with colorpicker

--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
+        title: Text('Namer App'),
         actions: [
           IconButton(
             onPressed: () {
